456da3e86ab7fb0b9152d546bfb6522f2c181019
Bogdan Rudko <rydkoc@icloud.com>
2022-05-19 14:38:52 +0300

    add: added exception for Skill

---
+++ src/main/java/com/ersted/exception/SkillAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.ersted.exception;
+
+public class SkillAlreadyExistException extends Exception {
+    public SkillAlreadyExistException(String message) {
+        super(message);
+    }
+}
