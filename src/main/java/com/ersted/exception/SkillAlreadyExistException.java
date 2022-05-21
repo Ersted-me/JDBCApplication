3488b24c994754508950cfb12c12f3381be3fb15
Bogdan Rudko <rydkoc@icloud.com>
2022-05-21 15:17:58 +0300

    review: exceptions changed.

--- src/main/java/com/ersted/exception/SkillAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.ersted.exception;
-
-public class SkillAlreadyExistException extends Exception {
-    public SkillAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/ersted/exception/SkillNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ersted.exception;
-
-public class SkillNotFoundException extends Exception {
-    public SkillNotFoundException(String message) {
-        super(message);
-    }
-}
