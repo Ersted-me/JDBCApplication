7000250c3cb9e249aa1a36d9ab6e7cb3728acf87
Bogdan Rudko <rydkoc@icloud.com>
2022-05-21 15:26:35 +0300

    add: added base and skill controller.

---
+++ src/main/java/com/ersted/controller/BaseController.java
@@ -0,0 +1,19 @@
+package com.ersted.controller;
+
+import com.ersted.exception.AlreadyExistException;
+import com.ersted.exception.NotFoundException;
+
+import java.util.List;
+
+public interface BaseController<T, R> {
+
+    T create(T obj) throws AlreadyExistException;
+
+    T getById(R id) throws NotFoundException;
+
+    T update(T obj) throws AlreadyExistException;
+
+    void deleteById(R id);
+
+    List<T> getAll();
+}
