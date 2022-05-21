8900faadbff09a574e42d9a6847686d44ad29304
Bogdan Rudko <rydkoc@icloud.com>
2022-05-21 15:25:42 +0300

    review: exception name changed.

--- src/main/java/com/ersted/service/BaseService.java
+++ src/main/java/com/ersted/service/BaseService.java
@@ -1,13 +1,16 @@
 package com.ersted.service;
 
+import com.ersted.exception.AlreadyExistException;
+import com.ersted.exception.NotFoundException;
+
 import java.util.List;
 
 public interface BaseService<T, R> {
     T create(T t);
 
-    T getById(R id) throws Exception;
+    T getById(R id) throws NotFoundException;
 
-    T update(T t) throws Exception;
+    T update(T t) throws AlreadyExistException;
 
     void deleteById(R id);
 
--- src/main/java/com/ersted/service/SkillService.java
+++ src/main/java/com/ersted/service/SkillService.java
@@ -1,8 +1,8 @@
 package com.ersted.service;
 
-import com.ersted.exception.SkillNotFoundException;
+import com.ersted.exception.NotFoundException;
 import com.ersted.model.Skill;
 
 public interface SkillService extends BaseService<Skill, Long>{
-    Skill getByName(String name) throws SkillNotFoundException;
+    Skill getByName(String name) throws NotFoundException;
 }
