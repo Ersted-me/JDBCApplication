b6368aed3218c24f1b392eba87e956924a18c370
Bogdan Rudko <rydkoc@icloud.com>
2022-06-08 15:04:25 +0300

    add: add equals and hashCode

--- src/main/java/com/ersted/model/Specialty.java
+++ src/main/java/com/ersted/model/Specialty.java
@@ -1,5 +1,7 @@
 package com.ersted.model;
 
+import java.util.Objects;
+
 public class Specialty extends BaseEntity {
     protected String specialty;
 
@@ -29,5 +31,18 @@ public class Specialty extends BaseEntity {
                 "id=" + id +
                 ", specialty='" + specialty + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Specialty specialty1 = (Specialty) o;
+        return this.specialty.equals(specialty1.getSpecialty()) && this.id.equals(specialty1.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(specialty);
     }
 }
